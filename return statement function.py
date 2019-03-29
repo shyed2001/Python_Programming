75ab78440c86b0ebf3013d2a68f5de73c240440b
shyed2001 <48835958+shyed2001@users.noreply.github.com>
2019-03-29 07:30:58 +0600

    Add files via upload

---
+++ return statement function.py
@@ -0,0 +1,45 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      User
+#
+# Created:     29/03/2019
+# Copyright:   (c) User 2019
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+print('''
+def cube1(num):
+    print(num*num*num)
+cube1(3)
+''')
+def cube1(num):
+    print(num*num*num)
+cube1(3)
+print('''
+def cube2(num):
+    return(num*num*num)
+print(cube2(2))
+''')
+def cube2(num):
+    return(num*num*num)
+print(cube2(2))
+print('''
+def cube3(num):
+    return num*num*num
+print(cube3(2))
+''')
+def cube3(num):
+    return num*num*num
+print(cube3(2))
+print('''
+def cube4(num):
+    return num*num*num
+result=cube4(4)
+print(result)
+''')
+def cube4(num):
+    return num*num*num
+result=cube4(4)
+print(result)
