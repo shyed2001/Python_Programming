86aaa311073f13026c8ef909f742aab302b7dc29
shyed2001 <48835958+shyed2001@users.noreply.github.com>
2019-04-09 01:59:34 +0600

    Add files via upload

--- turtle 1.py
+++ turtle 1.py
@@ -1,6 +1,3 @@
-print("import command packages")
-print("import turtle command drawing package, there are many pakages")
-
 import turtle
 tut = turtle.pen()
 tut = turtle.forward(200)
@@ -16,4 +13,5 @@ tut = turtle.right(20)
 tut = turtle.forward(120)
 tut = turtle.right(20)
 tut = turtle.back(120)
-quit()
+tut= exit()
+exit()
