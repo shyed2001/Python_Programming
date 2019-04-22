4fa0ab133bdf4b044c34d913e59a9035b60f5267
shyed2001 <48835958+shyed2001@users.noreply.github.com>
2019-04-22 20:19:06 +0600

    Add files via upload

--- CompoundedProfit.py
+++ CompoundedProfit.py
@@ -1,16 +1,16 @@
-print('''
+print("""
 Calculating:-
 A=Total money after years of compounded profit earnings
 P=Principal amount, the initial investment.
 r= Annual nominal interest rate, as decimal.
 n=Nuber of times the profit is compunded or added.
 t=number of yours
-Formula is A=P(1+(r/n))**(n*t) = A=(P+P*(r/n))**(n*t)
+Formula is A=P(1+(r/n))**(n*t)
 or
-a=(P+P*(r/n))
+a=(1+(r/n))
 b=(n*t)
-A=pow(a,b)
-''')
+A=P*a**b
+print(A)
 P=float(input("Please enter Principal amount the initial investment:  "))
 print("P=Principal amount, the initial investment.", P)
 r=float(input("Please enter Annual nominal interest rate, written decimal:  "))
@@ -19,14 +19,37 @@ n=float(input("Please enter Nuber of times the profit gets compunded/ added  per
 print("n Nuber of times the profit gets compunded/ added  per year ", n)
 t=float(input("Please enter, number of yours :   "))
 print("t number of yours  ", t)
-A1=(P+P*(r/n))**(n*t)
-print("A1=(P+P*(r/n))**(n*t) = ",A1)
+#A1=P(1+(r/n))**(n*t)
+print("A1= P(1+(r/n))**(n*t) = ", A)
 print('''
-a=(P+P*(r/n)
+a=(1+(r/n))
 b=(n*t)
-A2=pow(a,b)
-''')
-a=(P+P*(r/n))
-b=(n*t)
-A2=pow(a,b)
+A2=P*a**b
 print(A2)
+''')
+a=(1+(r/n))
+b=(n*t)
+A=P*a**b
+print(A)
+""")
+P=float(input("Please enter Principal amount the initial investment:  "))
+print("P=Principal amount, the initial investment.", P)
+r=float(input("Please enter Annual nominal interest rate, written decimal:  "))
+print(" r Annual nominal interest rate, written decimal ", r)
+n=float(input("Please enter Nuber of times the profit gets compunded/ added  per year:   "))
+print("n Nuber of times the profit gets compunded/ added  per year ", n)
+t=float(input("Please enter, number of yours :   "))
+print("t number of yours  ", t)
+#A1=P(1+(r/n))**(n*t)
+
+print('''
+a=(1+(r/n))
+b=(n*t)
+A2=P*a**b
+print(A2)
+''')
+a=(1+(r/n))
+b=(n*t)
+A=P*a**b
+print(A)
+print("A= P(1+(r/n))**(n*t) = ", A)
