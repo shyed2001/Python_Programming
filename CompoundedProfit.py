e8628b3fde416f91dd0c84ba1d95db3b0a6f06e0
shyed2001 <48835958+shyed2001@users.noreply.github.com>
2019-04-27 20:02:40 +0600

    Add files via upload

--- CompoundedProfit.py
+++ CompoundedProfit.py
@@ -53,3 +53,26 @@ b=(n*t)
 A=P*a**b
 print(A)
 print("A= P(1+(r/n))**(n*t) = ", A)
+
+def final_amount(p, r, n, t):
+    a = p * (1 + r/n) ** (n*t)
+    return a
+
+print("""
+def final_amount(p, r, n, t):
+    a = p * (1 + r/n) ** (n*t)
+    return a
+ Apply the compound interest formula to p
+to produce the final amount.
+toInvest = float(input("How much do you want to invest?"))
+fnl = final_amount(toInvest, 0.08, 12, 5)
+print("At the end of the period you'll have", fnl)
+
+""" )
+# This is new, and makes the function fruitful.
+# now that we have the function above, let us call it.
+
+toInvest = float(input("How much do you want to invest?"))
+fnl = final_amount(toInvest, 0.08, 12, 5)
+print("At the end of the period you'll have", fnl)
+
