bd67c45b28eb4c5ecd9b463d59956e388cd8b225
shyed2001 <48835958+shyed2001@users.noreply.github.com>
2019-04-29 14:47:07 +0600

    Add files via upload

---
+++ AreaCircle.py
@@ -0,0 +1,37 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      user
+#
+# Created:     29/04/2019
+# Copyright:   (c) user 2019
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+print("""
+def area_of_circle(r):
+    return (22/7)*(r)**2
+r=float(input("Please input the desired radious,r =  "))
+print(area_of_circle(r))
+
+
+def area_of_circle(r):
+     a= (22/7)*(r)**2
+     return a
+
+print(area_of_circle(7))
+""")
+def area_of_circle(r):
+    return (22/7)*(r)**2
+r=float(input("Please input the desired radious,r =  "))
+print(area_of_circle(r))
+
+
+def area_of_circle(r):
+     a= (22/7)*(r)**2
+     return a
+
+print(area_of_circle(7))
+
+
+
